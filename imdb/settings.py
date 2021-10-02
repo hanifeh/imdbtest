f340495110445ee438953de240c33ba52f7d3f7b
hanifeh <sadeghhanifehnia@gmail.com>
2021-10-02 14:23:18 +0330

    add review model

--- imdb/settings.py
+++ imdb/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'genre',
     'movie',
+    'review',
 ]
 
 MIDDLEWARE = [
---
+++ review/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from movie.models import Movie
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Review(models.Model):
+    """
+    model for one movie
+    """
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    writer = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    created_date = models.DateField(auto_now_add=True)
+    rate = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    body = models.TextField()
+
+    def __str__(self):
+        return self.writer
